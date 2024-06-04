f108a9303344174249e37d3e2f92b99675e914fe
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2024-06-03 23:51:30 -0500

    Added comment(s)

--- src/robot/mod.rs
+++ src/robot/mod.rs
@@ -14,6 +14,7 @@ use {
 const FIGHTER_ROBOT_INSTANCE_WORK_ID_INT_LAMP_EFEFCT_ID : i32 = 0x100000be;
 
 unsafe extern "C" fn robot_frame(fighter: &mut L2CFighterCommon) {
+    // To make this only work for certain slots, check Mod Workshop episode 24.
     let eff_handle = WorkModule::get_int(fighter.module_accessor, FIGHTER_ROBOT_INSTANCE_WORK_ID_INT_LAMP_EFEFCT_ID) as u32;
     if EffectModule::is_exist_effect(fighter.module_accessor, eff_handle) {
         EffectModule::kill(fighter.module_accessor, eff_handle, false, false);
